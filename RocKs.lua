eb72625963f0a2b41358f1d50daa644f4ce89a1b
ᴍᴏʀᴘʜɪɴᴇ <90528915+BLACKAAAAA@users.noreply.github.com>
2021-10-24 15:15:40 +0300

    Add files via upload

--- RocKs.lua
+++ RocKs.lua
@@ -8582,13 +8582,7 @@ tdcli_function ({ID = "SearchPublicChat",username_ = username}, start_function, 
 end
 if text == 'رتبتي' then
 local rtp = Rutba(msg.sender_user_id_,msg.chat_id_)
-local function getpro(extra, result, success)
-if result.photos_[0] then
-sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, result.photos_[0].sizes_[1].photo_.persistent_id_,"~رتبتك "..Rutba(msg.sender_user_id_,msg.chat_id_).." ✹💘", msg.id_, msg.id_, "md")
-else
-send(msg.chat_id_, msg.id_,'لا تمتلك صوره في حسابك', 1, 'md')
-  end end
-tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = msg.sender_user_id_, offset_ = 0, limit_ = 1 }, getpro, nil)
+send(msg.chat_id_, msg.id_,' *⌯︙رتبتك في البوت »* '..rtp)
 end
 if text == 'لقبه' and tonumber(msg.reply_to_message_id_) > 0 then
 function start_function(extra, result, success)
